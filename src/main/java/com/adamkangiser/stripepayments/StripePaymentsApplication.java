34b7d30493a45a8f477dae6dfb6e2de3a7e32648
adamk <adamkangiser@gmail.com>
2023-04-05 16:51:51 -0400

    2nd commit

--- src/main/java/com/adamkangiser/stripepayments/StripePaymentsApplication.java
+++ src/main/java/com/adamkangiser/stripepayments/StripePaymentsApplication.java
@@ -1,10 +1,18 @@
 package com.adamkangiser.stripepayments;
 
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StripePaymentsApplication {
+
+	@PostConstruct
+	public void setup() {
+		Stripe.apiKey = "sk_test_51MtcZbGbY7ZFyfeLZ8PEIcN8klSihBSWbPAqHeyBsOYoIljQL3lOZAehxtQbbRMoF1ew2DaM1rdKepyAP1sxGxmD00gIdFMAvX";
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StripePaymentsApplication.class, args);
---
+++ src/main/java/com/adamkangiser/stripepayments/dto/CreatePaymentResponse.java
@@ -0,0 +1,17 @@
+package com.adamkangiser.stripepayments.dto;
+
+public class CreatePaymentResponse {
+    private String clientSecret;
+
+    public CreatePaymentResponse(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
